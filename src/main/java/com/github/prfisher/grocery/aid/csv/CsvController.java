276ccb393e5ab003f79f46125408d7ffb6794052
prfisher <twoplasticforks@gmail.com>
2018-09-08 21:32:36 -0400

    Updated CsvController to return JSON instead of a page

--- src/main/java/com/github/prfisher/grocery/aid/csv/CsvController.java
+++ src/main/java/com/github/prfisher/grocery/aid/csv/CsvController.java
@@ -2,41 +2,26 @@ package com.github.prfisher.grocery.aid.csv;
 
 import javax.annotation.Resource;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.github.prfisher.grocery.aid.csv.CsvConverter;
 import com.github.prfisher.grocery.aid.models.GroceryList;
 
-@Controller
+@RestController
 @RequestMapping("/csv")
 public class CsvController {
-    @Value("${spring.application.name}")
-    String appName;
-
     @Resource
     CsvConverter converter;
 
-    @GetMapping
-    public String getUploadCsvPage(final Model model) {
-        model.addAttribute("appName", appName);
-        return "csv/upload";
-    }
-
     @PostMapping
-    public String uploadAndDisplayCsv(@RequestParam("file") MultipartFile file, final Model model) throws Exception {
-        model.addAttribute("appName", appName);
-
+    public GroceryList uploadAndDisplayCsv(@RequestParam("file") MultipartFile file) throws Exception {
         final GroceryList list = converter.apply(new String(file.getBytes()));
-        model.addAttribute("list", list);
         
-        return "csv/display";
+        return list;
     }
 }
 
